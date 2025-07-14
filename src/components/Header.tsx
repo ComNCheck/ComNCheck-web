f1666e42f7a8774bf241043099d30b194c026dcf
sungwonnoh <jessica20730@naver.com>
2025-07-14 11:55:30 +0900

    feat: 헤더바 ui제작

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,29 @@
+import { BiUserCircle } from "react-icons/bi";
+import logo from "../../public/images/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
-  return <header className="flex flex-col h-18 border-2"></header>;
+  return (
+    <header className="flex flex-col items-center justify-center h-18 text-[#64758B]">
+      <div className="mx-auto flex h-16 w-full items-center justify-between px-4 md:px-8 lg:w-[70%] ">
+        <Link href="/" className="relative h-12 w-28 shrink-0">
+          <Image
+            src={logo}
+            alt="로고"
+            fill
+            priority
+            sizes="(max-width: 768px) 112px, 140px"
+            className="object-contain"
+          />
+        </Link>
+        <div className="flex items-center justify-between font-semibold lg:text-xl text-sm lg:gap-4 gap-2">
+          <div>인수인계</div>
+          <div>행사일정</div>
+          <div>행사투표</div>
+          <BiUserCircle className="text-2xl" />
+        </div>
+      </div>
+    </header>
+  );
 }
