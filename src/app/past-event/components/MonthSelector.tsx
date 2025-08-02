9be4c6a412ee6f9d3be17cade504722e4d10e42f
sungwonnoh <jessica20730@naver.com>
2025-08-02 23:06:19 +0900

    feat(pastEvent): 월별ui 선택시 배경색 추가

--- src/app/past-event/components/MonthSelector.tsx
+++ src/app/past-event/components/MonthSelector.tsx
@@ -23,21 +23,56 @@ export default function MonthSelector({ onMonthSelect }: MonthSelectorProps) {
     onMonthSelect(group);
   };
 
+  const renderedMonths = new Set<number>();
+
   return (
     <div className="flex flex-wrap gap-2">
       {months.map((month) => {
-        const isSelected = selectedMonth.includes(month);
-        return (
-          <div
-            key={month}
-            onClick={() => handleMonthClick(month)}
-            className={`text-center p-2 w-16 h-9  text-sm font-semibold rounded-[50px] ${
-              isSelected ? "bg-point text-white" : "bg-[#E3E3E3] text-[#3a3a3a]"
-            }`}
-          >
-            {month}월
-          </div>
-        );
+        if (renderedMonths.has(month)) return null;
+        const group = groupedMonths.find((g) => g.includes(month));
+        if (group) {
+          group.forEach((m) => renderedMonths.add(m));
+          const isSelected = selectedMonth.toString() === group.toString();
+          return (
+            <div
+              key={`group-${group[0]}`}
+              className={`flex gap-2 rounded-xl cursor-pointer ${
+                isSelected ? "bg-sublight" : "bg-white"
+              }`}
+              onClick={() => handleMonthClick(month)}
+            >
+              {group.map((m) => (
+                <div
+                  key={m}
+                  className={`text-center w-16 h-9 text-sm font-semibold rounded-[50px] flex items-center justify-center ${
+                    isSelected
+                      ? "bg-point text-white"
+                      : "bg-[#E3E3E3] text-[#3a3a3a]"
+                  }`}
+                >
+                  {m}월
+                </div>
+              ))}
+            </div>
+          );
+        } else {
+          // 단일 월 렌더링
+          const isSelected = selectedMonth.includes(month);
+          renderedMonths.add(month);
+          return (
+            <div
+              key={month}
+              onClick={() => handleMonthClick(month)}
+              className={`text-center w-16 h-9 text-sm font-semibold rounded-[50px] flex items-center justify-center cursor-pointer ${
+                isSelected
+                  ? "bg-point text-white"
+                  : "bg-[#E3E3E3] text-[#3a3a3a]"
+              }`}
+            >
+              {month}월
+            </div>
+          );
+        }
       })}
     </div>
   );
