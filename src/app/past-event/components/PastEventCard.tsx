5fb9380059cab6db2996c9eb55f7ff2a743a0caf
sungwonnoh <jessica20730@naver.com>
2025-08-10 21:22:45 +0900

    fix: 행사에 따른 체크리스트 갱신

--- src/app/past-event/components/PastEventCard.tsx
+++ src/app/past-event/components/PastEventCard.tsx
@@ -1,14 +1,19 @@
 interface TitleAndDescriptionProps {
   title: string;
   description: string;
+  onClick?: () => void;
 }
 
 const PastEventCard: React.FC<TitleAndDescriptionProps> = ({
   title,
   description,
+  onClick,
 }) => {
   return (
-    <div className="w-full bg-[#EFF7FF] text-black rounded-2xl px-4 py-3 my-2 md:px-6 md:py-4 min-h-[6rem] flex flex-col justify-center">
+    <div
+      onClick={onClick}
+      className="w-full bg-[#EFF7FF] text-black rounded-2xl px-4 py-3 my-2 md:px-6 md:py-4 min-h-[6rem] flex flex-col justify-center"
+    >
       <h3 className="text-xl md:text-2xl font-extrabold mb-1">| {title}</h3>
       <div className="text-base font-medium leading-relaxed">{description}</div>
     </div>
