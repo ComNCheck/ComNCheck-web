a7a3b4f744750c119cf3714a8ba1ef2f0eee41ab
sungwonnoh <jessica20730@naver.com>
2025-08-17 12:06:10 +0900

    feat: 구글 로그인 연결

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { BiUserCircle } from "react-icons/bi";
-import logo from "../../public/images/logo.png";
+import logo from "../../public/images/logo.png"
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Header() {
   const router = useRouter();
+  const handleLogin = () => {
+    console.log("Google 로그인 시도 중...");
+    const baseURL = process.env.NEXT_PUBLIC_API_URL;
+    window.location.href = `${baseURL}/oauth2/authorization/google`;
+  };
   return (
     <header className="fixed inset-x-0 top-0 flex flex-col items-center justify-center h-18 text-[#64758B] bg-white z-100">
       <div className="mx-auto flex h-16 w-full items-center justify-between px-4 md:px-8 lg:w-[70%] ">
@@ -24,7 +29,7 @@ export default function Header() {
           <div onClick={() => router.push("/past-event")}>인수인계</div>
           <div onClick={() => router.push("/calendar")}>행사일정</div>
           <div onClick={() => router.push("/popularEvent")}>행사투표</div>
-          <BiUserCircle className="text-xl" />
+          <BiUserCircle className="text-xl" onClick={handleLogin}/>
         </div>
       </div>
     </header>
