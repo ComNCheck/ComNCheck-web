eba5c5befcd9360a49cdedfcf94f77807011ace4
sungwonnoh <jessica20730@naver.com>
2025-08-22 09:47:45 +0900

    feat: 남은 일정 퍼센트 계산 로직

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -7,6 +7,8 @@ import { LeftEventCount } from "@/apis/event.type";
 
 export default function Main() {
   const [count, setCount]=useState<LeftEventCount | null>(null);
+  const [percentPassed, setPercentPassed] = useState<number>(0);
+
   useEffect(()=>{
     const fetchCount = async()=>{
       try{
@@ -16,13 +18,27 @@ export default function Main() {
         console.error("남은 과행사 개수 조회 실패", error);
       }
     }
+    const calculateYearPassed = () => {
+      const now = new Date();
+      const startOfYear = new Date(now.getFullYear(), 0, 0);
+      const diff = now.getTime() - startOfYear.getTime();
+      const dayOfYear = Math.floor(diff / (1000 * 60 * 60 * 24));
+      
+      const year = now.getFullYear();
+      const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+      const totalDaysInYear = isLeapYear ? 366 : 365;
+
+      const percentage = Math.floor((dayOfYear / totalDaysInYear) * 100);
+      setPercentPassed(percentage);
+    };
     fetchCount();
+    calculateYearPassed();
   },[])
   return (
     <div className="min-h-screen text-black bg-white flex flex-col items-left justify-left py-12">
       <div className="w-full h-full max-w-5xl mx-auto px-8">
         <TitleAndDescription
-          title={"이예림님,\n올해가 58% 지나갔어요!"}
+          title={`이예림님,\n올해가 ${percentPassed}% 지나갔어요!`}
           description={
             `앞으로 남은 행사는 ${count?.count}개 입니다. 오늘도 학과를 위해 힘써주셔서 감사합니다.`
           }
