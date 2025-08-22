110fb023dce9bb3356ea0d7d99a21f6ac249a12b
sungwonnoh <jessica20730@naver.com>
2025-08-22 09:56:58 +0900

    feat: 멤버 정보 가져오기 api

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -4,16 +4,20 @@ import TitleAndDescription from "../../components/TitleAndDescription";
 import EventCardList from "./components/EventCardList";
 import { getCountLeftEvent } from "@/apis/event";
 import { LeftEventCount } from "@/apis/event.type";
+import { getMemberData } from "@/apis/member";
 
 export default function Main() {
   const [count, setCount]=useState<LeftEventCount | null>(null);
+  const [memberName, setMemberName]=useState<string>("");
   const [percentPassed, setPercentPassed] = useState<number>(0);
 
   useEffect(()=>{
     const fetchCount = async()=>{
       try{
         const data = await getCountLeftEvent();
+        const name= await getMemberData();
         setCount(data);
+        setMemberName(name.name); 
       }catch(error){
         console.error("남은 과행사 개수 조회 실패", error);
       }
@@ -38,7 +42,7 @@ export default function Main() {
     <div className="min-h-screen text-black bg-white flex flex-col items-left justify-left py-12">
       <div className="w-full h-full max-w-5xl mx-auto px-8">
         <TitleAndDescription
-          title={`이예림님,\n올해가 ${percentPassed}% 지나갔어요!`}
+          title={`${memberName}님,\n올해가 ${percentPassed}% 지나갔어요!`}
           description={
             `앞으로 남은 행사는 ${count?.count}개 입니다. 오늘도 학과를 위해 힘써주셔서 감사합니다.`
           }
